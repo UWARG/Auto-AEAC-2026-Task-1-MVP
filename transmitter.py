e94a0b9274fc06f24b67191c0517664d1d0c8364
Tochi <tochiokoro2001@gmail.com>
2026-03-14 18:11:33 -0400

    Added manual exposure setting

--- transmitter.py
+++ transmitter.py
@@ -229,6 +229,8 @@ class OakCamera:
         left.setFps(20)
         right.setFps(20)
 
+        rgb.initialControl.setManualExposure(500, 50)
+
         stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.DENSITY)
         stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
         stereo.setOutputSize(self.width, self.height)
